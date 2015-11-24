617722230f8af50ca45e738324242ab1e5858981
ghaiklor <ghaiklor@gmail.com>
2015-11-24 18:13:42 +0200

    feat(app): Removes export object literal from module
    When you export object literal as default export it doesn't follow the spec

--- src/app/templates/test/unit/index.test.js
+++ src/app/templates/test/unit/index.test.js
@@ -1,8 +1,1 @@
 import { assert } from 'chai';
-import module from '../../src/index';
-
-describe('Entry Point', () => {
-  it('Should properly export', () => {
-    assert.isObject(module);
-  });
-});
